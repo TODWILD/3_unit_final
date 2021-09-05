399e054fe3d31794162e8d5327721e69c41bed0f
TODWILD <87666407+TODWILD@users.noreply.github.com>
2021-09-05 23:08:45 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,9 @@
-import time
-def test_eng_should_see_add_button(browser): # В фикстуру передаём аргумент browser из нашего conftest
+def test_eng_add_button_should_be_seen(browser):
+
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30) # Ставим паузу в 30 сек как и сказано в задании
+    time.sleep(30)
+    add_button = browser.find_element_by_class_name(".btn-add-to-basket").is_enabled()
 
-
-    add_button = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket").is_enabled()
-    # Ищем уникальный селектор для кнопки
-
-    assert add_button, "button is missing" # проверяем что кнопка присутствует на странице
+    assert add_button, "Отсутствует кнопка добавления товара в корзину"
+    

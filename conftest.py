093f81a21122a3dda277593d94a8efda887d9ae3
TODWILD <87666407+TODWILD@users.noreply.github.com>
2021-09-05 21:48:42 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,22 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",help="Choose language: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
+
+@pytest.fixture()
+def language1(request):
+    language = request.config.getoption("language")
+    return language
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(language1):
+    if str(language1) in "ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    else:
+        raise pytest.UsageError("--language should be one of: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
     yield browser
     print("\nquit browser..")
     browser.quit()

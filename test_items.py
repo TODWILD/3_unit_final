40ef054f37d86f9fa9fb35c513a9eb6b998bae3b
TODWILD <87666407+TODWILD@users.noreply.github.com>
2021-09-07 20:57:19 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,11 +3,11 @@ def test_eng_add_button_should_be_seen(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(1) 
-    a1=browser.find_element(By.CSS_SELECTOR, "[value={}]".format(str(language1)))#выбор языка
-    a1.click()
+    l=browser.find_element(By.CSS_SELECTOR, "[value={}]".format(str(language1)))#выбор языка
+    l.click()
     time.sleep(3)
-    but=browser.find_element_by_css_selector("button.btn.btn-default")#смена языка на сайте
-    but.click()
+    button=browser.find_element_by_css_selector("button.btn.btn-default")#смена языка на сайте
+    button.click()
     time.sleep(10)
     add_button = browser.find_element_by_class_name(".btn-add-to-basket").is_enabled()
 

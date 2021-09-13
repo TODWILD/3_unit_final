21b2c39ca8e76e1505477cb819202d9fc991dfb4
TODWILD <87666407+TODWILD@users.noreply.github.com>
2021-09-13 22:57:19 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,22 +1,40 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+LANGUAGES = ('ar', 'ca', 'cs', 'da', 'de', 'en', 'el', 'es', 'fi', 'fr', 'it',
+             'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh')
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",help="Choose language: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store',
+                     help=f'Choose language from {LANGUAGES}')
 
-@pytest.fixture()
-def language1(request):
-    language = request.config.getoption("language")
-    return language
 
-@pytest.fixture(scope="function")
-def browser(language1):
-    if str(language1) in "ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
+
+    if user_language not in LANGUAGES:
+        raise pytest.UsageError(f'--language should be from list {LANGUAGES}')
+
+    if browser_name == 'chrome':
+        print('\nstart chrome browser for test..')
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print('\nstart firefox browser for test..')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--language should be one of: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
-    yield browser
-    print("\nquit browser..")
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
+
+    yield browser, user_language
+    print('\nquit browser..')
     browser.quit()
